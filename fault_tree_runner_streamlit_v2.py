ef6a4a569a3570d245466583199264a96aa01590
Srini <Srinivas.neppali@gmail.com>
2025-11-22 14:56:15 +0530

    Auto-commit from script at 2025-11-22 14:56:15

--- fault_tree_runner_streamlit_v2.py
+++ fault_tree_runner_streamlit_v2.py
@@ -1239,22 +1239,6 @@ if go_back and len(st.session_state.visited_stack) > 1:
 
 if go_next:
 
-# --- Custom Spinner Color CSS ---
-st.markdown("""
-            <style>
-            div[data-testid="stSpinner"] svg {
-            stroke: #00c8ff !important;
-            }
-            div[data-testid="stSpinner"] svg {
-            width: 36px !important;
-            height: 36px !important;
-            }
-            div[data-testid="stSpinner"] svg circle {
-                stroke-width: 4 !important;
-            }
-            </style>
-            """, unsafe_allow_html=True)
-
     with st.spinner("🚀 Syncing your step with Jeeves Cloud..."):
         st.markdown(
             "<div class='spinner-tip'>✨ Uploading evidence, updating logs, and loading the next action...</div>",
